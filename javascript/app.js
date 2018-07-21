cbb4c0fd22ac66cac09c2a47802ef5d773eae4f6
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-07-21 11:44:20 -0400

    Hard coded 'rated' and 'state' to queryURL. Users will only see charities that are rated and in North Carolina

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 
-   var state = "&state=NC";
    var city = "&city=Durham";
    var rated = "&rated=TRUE";
 
 
-   var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=8a6cb061&app_key=2314291975d64eb1828a504d597b3b47" + state + city + rated; 
+   var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=8a6cb061&app_key=2314291975d64eb1828a504d597b3b47&rated=TRUE&state=NC" + city; 
 
     $.ajax({
         url: queryURL,
