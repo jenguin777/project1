3a3fa5e88ab747f68a37f793f4cd92ebacc18d2a
Pack Fan 1031 <ocskier@gmail.com>
2018-07-23 03:30:03 -0400

    latest revision

--- js/init.js
+++ js/init.js
@@ -1,8 +1,10 @@
 (function($){
+
   $(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    M.AutoInit();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
