183a04a4d3ada291bfa401207969779286bcec1b
Pack Fan 1031 <ocskier@gmail.com>
2018-07-25 18:55:22 -0400

    latest revision

--- js/app.js
+++ js/app.js
@@ -11,14 +11,20 @@ $(document).ready(function() {
     //Religion = 9
     //Research and Public Policy = 11
 
-    var categoryImages = ["Animals1.jpg","ArtsCultureHumanities2.jpg","Education3.jpg","Environment4.jpg","Health5.jpg","HumanServices6.jpg","HumanCivilRights8.jpg","Religion9.jpg","ResearchPublicPolicy11.jpg"];
+    var categoryImages = [{category:"Animals",url:"Animals1.jpg"},{category:"ArtsCultureHumanities",url:"ArtsCultureHumanities2.jpg"},
+                            {category:"Education",url:"Education3.jpg"},{category:"Environment",url:"Environment4.jpg"},
+                            {category:"Health",url:"Health5.jpg"},{category:"HumanServices",url:"HumanServices6.jpg"},
+                            {category:"HumanCivilRights",url:"HumanCivilRights8.jpg"},{category:"Religion",url:"Religion9.jpg"},
+                            {category:"ResearchPublicPolicy",url:"ResearchPublicPolicy11.jpg"}];
+    
+    console.log(categoryImages);
     for (var i=0;i<categoryImages.length;i++) {
         var catDiv = $('<div class="col s4 m3" category>');
         var cardA = $('<a href="results.html" target=""></a>');
         var cardDiv = $('<div class="card small"  style="height: 120px;"></div>');
         var cardImg = $('<div class="card-image" style="max-height: 100%; overflow: none;"></div>');              
-        cardImg.append('<img class="responsive-img" src="images/'+categoryImages[i]+'">');
-        cardImg.append('<span class="card-title">Animals</span>');
+        cardImg.append('<img class="responsive-img" src="images/'+categoryImages[i].url+'">');
+        cardImg.append('<span class="card-title">'+categoryImages[i].category+'</span>');
         cardDiv.append(cardImg);
         cardA.append(cardDiv);
         catDiv.append(cardA);
@@ -59,7 +65,7 @@ $(document).ready(function() {
                 //Card Div
                 var $mycard = $('<div class="card" style="height:275px"></div>');
 
-                var $cardContent = $('<div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/'+categoryImages[i] +'" style="height:130px";></div>');
+                var $cardContent = $('<div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/'+categoryImages[i].url +'" style="height:130px";></div>');
 
                 var $cardContentAction = $('<div class="card-action" style="height:40%;padding: 5px 18px;">');
                 
