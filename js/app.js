16f9980019fb2a5c3a4e65dcbba309dffc443a19
Pack Fan 1031 <ocskier@gmail.com>
2018-07-27 07:48:10 -0400

    latest revision

--- js/app.js
+++ js/app.js
@@ -17,11 +17,28 @@ $(document).ready(function() {
                             {category:"HumanCivilRights",url:"HumanCivilRights8.jpg",catNum:"8"},{category:"Religion",url:"Religion9.jpg",catNum:"9"},
                             {category:"ResearchPublicPolicy",url:"ResearchPublicPolicy11.jpg",catNum:"11"}];
     
+
+    var config = {
+        apiKey: "AIzaSyB3OTKnscA9uQXfdcKUkuPOANkEF-lUVA0",
+        authDomain: "projectcodingcamp.firebaseapp.com",
+        databaseURL: "https://projectcodingcamp.firebaseio.com",
+        projectId: "projectcodingcamp",
+        storageBucket: "projectcodingcamp.appspot.com",
+        messagingSenderId: "277978229879"
+        };
+        
+    // Inititalize the database
+    firebase.initializeApp(config);
+    
+    // Assign my database ref for folder TrainScheduler to a variable
+    var db = firebase.database();
+    var jjdb = db.ref("Project1");
+    
     console.log(categoryImages);
     for (var i=0;i<categoryImages.length;i++) {
-        var catDiv = $('<div class="col s4 m3 category" data="'+categoryImages[i].catNum+'">');
+        var catDiv = $('<div class="col s6 m4 category" data="'+categoryImages[i].catNum+'">');
         var cardA = $('<a href="results.html" target="_blank"></a>');
-        var cardDiv = $('<div class="card small"  style="height: 120px;"></div>');
+        var cardDiv = $('<div class="card small"  style="height: 120px;border: 2px solid darkgray;border-radius: 7px"></div>');
         var cardImg = $('<div class="card-image" style="max-height: 100%; overflow: none;"></div>');              
         cardImg.append('<img class="responsive-img" src="images/'+categoryImages[i].url+'">');
         cardImg.append('<span class="card-title">'+categoryImages[i].category+'</span>');
@@ -30,7 +47,96 @@ $(document).ready(function() {
         catDiv.append(cardA);
         $("#catRow").append(catDiv);
     }
+
+    function makeFavCard (favName,favPic,favStat,favUrl) {
+        jjdb.push ({
+            FavCharity : favName,
+            CharityPic : favPic,
+            CharStat : favStat,
+            CharUrl : favUrl
+        });
+      }
+
+    var lengthFavList=0;
+
+    jjdb.on("child_added", function(childSnapshot) {
+
+        lengthFavList++;
+        var $npDiv = $('<div id="indexFav-'+lengthFavList+'" class="col s6 m4 cardcol for-buttons" data-Name="'+childSnapshot.val().FavCharity+'"></div>');
+
+        //Card Div
+        var $mycard = $('<div class="card" style="height:275px"></div>');
+
+        var $cardContent = $('<div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+childSnapshot.val().CharityPic +'" style="height:130px";></div>');
+
+        var $cardContentAction = $('<div class="card-action" style="height:40%;padding: 5px 18px;">');
+        
+        var $cardContentSpan=$('<p class="card-title activator grey-text text-darken-4" style="font-size:20px;line-height:1.4rem;" ></span>');
+        $cardContentSpan.text(childSnapshot.val().FavCharity);
+
+        var $icon = $("<i>");
+        $icon.addClass("material-icons right");
+        $icon.text("more_vert");
+
+        $cardContentSpan.append($icon);
+        $cardContentAction.append($cardContentSpan);
+
+        //Card Reveal Creation
+        var $cardReveal = $("<div>");
+        $cardReveal.addClass("card-reveal");
+
+        var $cardRevealSpan = $("<p>");
+        $cardRevealSpan.addClass("card-title grey-text text-darken-4");
+        $cardRevealSpan.css({"font-size":"16px","line-height":"1.4rem" });
+        $cardRevealSpan.append(childSnapshot.val().FavCharity);
+
+        var $cardRevealIcon = $("<i>");
+        $cardRevealIcon.addClass("material-icons right");
+        $cardRevealIcon.css({"margin-left":"0","line-height":"1.25" });
+        $cardRevealIcon.text("close");
+
+        $cardRevealSpan.append($cardRevealIcon);
+
+        var $cardRevealP = $("<p>");
+        $cardRevealP.text(childSnapshot.val().favStat);
+        var $cardRevealP2 = $("<p>");
+        var $cardRevealLink1 = $('<a href="'+childSnapshot.val().CharUrl+'" target="_blank" style="color:fuchsia">');
+        $cardRevealLink1.text("Contribute");
+        $cardRevealP2.append($cardRevealLink1);
+        var $cardRevealP3 = $("<p>");
+        var $cardRevealBtn = $('<button class="checkbox" data="'+ lengthFavList+'"></button>"');
+        $cardRevealBtn.text("X");
+        $cardRevealP3.append($cardRevealBtn);
+
+        $cardReveal.append($cardRevealSpan).append($cardRevealP).append($cardRevealP2).append($cardRevealP3);
+
+        // Appending CardImg, CardContent, and Card Reveal to its div
+        $mycard.append($cardContent);
+        $mycard.append($cardContentAction);
+        $mycard.append($cardReveal);
+        $npDiv.append($mycard);
+        
+        $("#favRow").append($npDiv);   
+        // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
     
+    $("#favRow").on("click", ".checkbox", function () {
+        var indexFavClicked = $(this).attr("data");
+        var del_name = $("#indexFav-"+indexFavClicked).attr("data-Name");
+        $("#indexFav-"+indexFavClicked).remove();
+        console.log(del_name);
+        
+        jjdb.orderByChild('FavCharity').equalTo(del_name)
+            .once('value').then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+            //remove each child
+            jjdb.child(childSnapshot.key).remove();
+            });
+        });
+    });
+
     //    var city = "";
 
        var state = "";
@@ -60,7 +166,7 @@ $(document).ready(function() {
 
             for (var i = 0; i <data.length; i++) {
                 //most-outer Div
-                var $npDiv = $('<div class="col s6 m4 cardcol for-buttons" data-Name="'+data[i].charityName+'"></div>');
+                var $npDiv = $('<div id="index-'+i+'" class="col s6 m4 cardcol for-buttons" data-Name="'+data[i].charityName+'"></div>');
 
                 //Card Div
                 var $mycard = $('<div class="card" style="height:275px"></div>');
@@ -96,13 +202,13 @@ $(document).ready(function() {
                 $cardRevealSpan.append($cardRevealIcon);
 
                 var $cardRevealP = $("<p>");
-                $cardRevealP.text("data[i].rating");
+                $cardRevealP.text(data[i].irsClassification.deductibility);
                 var $cardRevealP2 = $("<p>");
                 var $cardRevealLink1 = $('<a href="'+data[i].websiteURL+'" target="_blank" style="color:fuchsia">');
                 $cardRevealLink1.text("Contribute");
                 $cardRevealP2.append($cardRevealLink1);
                 var $cardRevealP3 = $("<p>");
-                var $cardRevealBtn = $('<button class="favChar" data-Name="'+data[i].charityName.replace(/\s+/g, '')+'"></button>');
+                var $cardRevealBtn = $('<button data="'+i+'" class="favChar"></button>');
                 $cardRevealBtn.text("My Fav");
                 $cardRevealP3.append($cardRevealBtn);
 
@@ -122,6 +228,7 @@ $(document).ready(function() {
 
         callCharApi(categoryId);
 
+
         // Click handler for adding a gif to favorites
         $("#catRow").on("click", ".category", function () {
 
@@ -136,11 +243,15 @@ $(document).ready(function() {
         $(".char-card").on("click", ".favChar", function () {
 
             // Grab the index info
-            var nameClicked = $(this).attr("data-Name");
-            console.log(nameClicked);
+        var nameClicked = $(this).attr("data");
+        console.log(nameClicked);
             // Remove the gif from search
-            var copyCard = $("#"+nameClicked);
-            console.log(copyCard);
+        var copyName = $("#index-"+nameClicked).attr("data-Name");
+        var copyPic = $("#index-"+nameClicked+ ' .card .card-image img').attr("src");
+        var copyStat = $("#index-"+nameClicked+ ' .card .card-reveal :nth-child(2)').text();
+        var copyUrl = $("#index-"+nameClicked+ ' .card .card-reveal p a').attr("href");
+        makeFavCard(copyName,copyPic,copyStat,copyUrl);
+
             // Copy it to favorites container
         });
     });
